b52b7061b68cba304b12487bb94e78f0d830bd88
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2020-11-19 22:03:17 +0300

    Update CheckFormTests.java

--- src/test/java/CheckFormTests.java
+++ src/test/java/CheckFormTests.java
@@ -1,134 +1,81 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
-
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.datatransfer.StringSelection;
 import java.io.File;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CheckFormTests {
 
     String firstName = "Ivan";
     String lastName = "Ivanov";
     String mail = "ivan@mail.com";
+    String gender = "Male";
     String phoneNumber = "8911321127";
     String date = "18";
-    String month = "3";
+    String month = "April";
     String year = "1985";
-    String subjects = "English, History";
+    String subject1 = "English";
+    String subject2 = "History";
+    String hobbi1 = "Sports";
+    String hobbi2 = "Reading";
+    String hobbi3 = "Music";
     String currentAddress = "Russia, Saint-Petersburg, Mira st., 18";
     String state = "Uttar Pradesh";
     String city = "Agra";
     String fileName = "1.png";
 
-    public static void setClipboardData(String string) {
-        StringSelection stringSelection = new StringSelection(string);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-    }
-
-//    @BeforeAll
-//    static void setUp() {
-//        Configuration.startMaximized = true;
-//        Configuration.browser = "chrome";
-//    }
-
-    @Order(0)
     @Test
     public void fillForm() throws AWTException {
+//        Заполнение и сохранение формы
         Configuration.startMaximized = true;
         open("https://demoqa.com/automation-practice-form");
-        $x("//input[@id='firstName']").setValue(firstName);
-        $x("//input[@id='lastName']").setValue(lastName);
-        $x("//input[@id='userEmail']").setValue(mail);
-        $x("//label[@for='gender-radio-1']").click();
-        $x("//input[@id='userNumber']").setValue(phoneNumber);
-        $x("//input[@id='dateOfBirthInput']").click();
-        $x("//select[@class='react-datepicker__year-select']").click();
-        $x("//select[@class='react-datepicker__year-select']/option[@value='" + year + "']").click();
-        $x("//select[@class='react-datepicker__month-select']").click();
-        $x("//select[@class='react-datepicker__month-select']/option[@value='" + month + "']").click();
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(mail);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month-select").selectOption(month);
         $x("//div[@class='react-datepicker__month']//div[contains(text(),'" + date + "')]").click();
-        $x("//input[@id='subjectsInput']").setValue(subjects);
-        $x("//label[@for='hobbies-checkbox-2']").click();
-        $x("//label[@for='hobbies-checkbox-3']").click();
-
-        $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/" + fileName));
-
-        $x("//textarea[@id='currentAddress']").setValue(currentAddress);
+        $("#subjectsInput").setValue(subject1).pressEnter();
+        $("#subjectsInput").setValue(subject2).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbi1)).click();
+        $("#hobbiesWrapper").$(byText(hobbi2)).click();
+        $("#hobbiesWrapper").$(byText(hobbi3)).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/" + fileName));
+        $("#currentAddress").setValue(currentAddress);
         $x("//div[@id='state']//input").setValue(state).pressEnter();
         $x("//div[@id='city']//input").setValue(city).pressEnter();
-        $x("//button[@id='submit']").click();
+        $("#submit").click();
         $x("//div[@id='example-modal-sizes-title-lg']").shouldHave(Condition.text("Thanks for submitting the form"));
-
-    }
-
-    @Test
-    @Order(1)
-    public void checkName() {
+//      Проверка имени и фамилии
         $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(firstName));
         $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(lastName));
-    }
-
-    @Test
-    @Order(2)
-    public void checkMail() {
+//      Проверка почты
         $x("//tbody//td[contains(text(),'Student Email')]/../td[2]").shouldHave(Condition.text(mail));
-    }
-
-    @Test
-    @Order(3)
-    public void CheckGender() {
+//      Проверка пола
         $x("//tbody//td[contains(text(),'Gender')]/../td[2]").shouldHave(Condition.text("Male"));
-    }
-
-    @Test
-    @Order(4)
-    public void checkPhone() {
+//        Проверка номера телефона
         $x("//tbody//td[contains(text(),'Mobile')]/../td[2]").shouldHave(Condition.text(phoneNumber));
-    }
-
-    @Test
-    @Order(5)
-    public void checkBirth() {
+//        Проверка даты рождения
         $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(date));
-        $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text("April"));
+        $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(month));
         $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(year));
-    }
-
-    @Test
-    @Order(6)
-    public void checkSubjects() {
-        $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subjects));
-    }
-
-    @Test
-    @Order(7)
-    public void checkHobbies() {
-        $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text("Reading"));
-        $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text("Music"));
-    }
-
-//    Тут еще нужна проверка на наличие имени прикрепленного файла
-    @Test
-    @Order(8)
-    public void checkAddress() {
-        $x("//tbody//td[contains(text(),'Address')]/../td[2]").shouldHave(Condition.text(currentAddress));
-    }
-
-    @Test
-    @Order(9)
-    public void checkFileName() {
+//        Проверка предметов
+        $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subject1));
+        $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subject2));
+//        Проверка выбранных хобби
+        $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobbi1));
+        $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobbi2));
+        $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobbi3));
+//        Проверка на наличие имени прикрепленного файла
         $x("//tbody//td[contains(text(),'Picture')]/../td[2]").shouldHave(Condition.text(fileName));
-    }
-
-    @Test
-    @Order(10)
-    public void checkStateAndCity() {
+//        Проверка адреса
+        $x("//tbody//td[contains(text(),'Address')]/../td[2]").shouldHave(Condition.text(currentAddress));
+//        Проверка штата и города
         $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(state));
         $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(city));
     }
