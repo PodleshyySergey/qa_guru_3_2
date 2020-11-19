6595cfd94129f302d722853c197d5a3d26cadbf2
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2020-11-19 11:33:47 +0300

    Правка загрузки файла и попытка исправления с @Order

--- src/test/java/CheckFormTests.java
+++ src/test/java/CheckFormTests.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.datatransfer.StringSelection;
+import java.io.File;
 
 public class CheckFormTests {
 
@@ -21,15 +22,15 @@ public class CheckFormTests {
     String currentAddress = "Russia, Saint-Petersburg, Mira st., 18";
     String state = "Uttar Pradesh";
     String city = "Agra";
-    String fileName = "2020.11.13_001.png";
+    String fileName = "1.png";
 
     public static void setClipboardData(String string) {
         StringSelection stringSelection = new StringSelection(string);
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
     }
 
+    @Order(1)
     @Test
-    @Order(0)
     public void fillForm() throws AWTException {
         open("https://demoqa.com/automation-practice-form");
         $x("//input[@id='firstName']").setValue(firstName);
@@ -47,20 +48,7 @@ public class CheckFormTests {
         $x("//label[@for='hobbies-checkbox-2']").click();
         $x("//label[@for='hobbies-checkbox-3']").click();
 
-//        Тут еще нужно файл выбрать и видимо запомнить его имя для проверки
-//        $x("//input[@id='uploadPicture']").sendKeys("C:/Users/UserDesktop/Временная/2020.11.13/2020.11.13_001.png");
-
-        $x("//label[@for='uploadPicture']").click();
-        setClipboardData("C:\\Users\\User\\Desktop\\Временная\\2020.11.13\\" + fileName);
-        //native key strokes for CTRL, V and ENTER keys
-        Robot robot = new Robot();
-        robot.delay(1000);
-        robot.keyPress(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_V);
-        robot.keyRelease(KeyEvent.VK_CONTROL);
-        robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);
+        $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/" + fileName));
 
         $x("//textarea[@id='currentAddress']").setValue(currentAddress);
         $x("//div[@id='state']//input").setValue(state).pressEnter();
@@ -71,32 +59,32 @@ public class CheckFormTests {
     }
 
     @Test
-    @Order(1)
+    @Order(2)
     public void checkName() {
         $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(firstName));
         $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(lastName));
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     public void checkMail() {
         $x("//tbody//td[contains(text(),'Student Email')]/../td[2]").shouldHave(Condition.text(mail));
     }
 
     @Test
-    @Order(3)
+    @Order(4)
     public void CheckGender() {
         $x("//tbody//td[contains(text(),'Gender')]/../td[2]").shouldHave(Condition.text("Male"));
     }
 
     @Test
-    @Order(4)
+    @Order(5)
     public void checkPhone() {
         $x("//tbody//td[contains(text(),'Mobile')]/../td[2]").shouldHave(Condition.text(phoneNumber));
     }
 
     @Test
-    @Order(5)
+    @Order(6)
     public void checkBirth() {
         $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(date));
         $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text("April"));
@@ -104,13 +92,13 @@ public class CheckFormTests {
     }
 
     @Test
-    @Order(6)
+    @Order(7)
     public void checkSubjects() {
         $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subjects));
     }
 
     @Test
-    @Order(7)
+    @Order(8)
     public void checkHobbies() {
         $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text("Reading"));
         $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text("Music"));
@@ -118,19 +106,19 @@ public class CheckFormTests {
 
 //    Тут еще нужна проверка на наличие имени прикрепленного файла
     @Test
-    @Order(8)
+    @Order(9)
     public void checkAddress() {
         $x("//tbody//td[contains(text(),'Address')]/../td[2]").shouldHave(Condition.text(currentAddress));
     }
 
     @Test
-    @Order(8)
+    @Order(10)
     public void checkFileName() {
         $x("//tbody//td[contains(text(),'Picture')]/../td[2]").shouldHave(Condition.text(fileName));
     }
 
     @Test
-    @Order(9)
+    @Order(11)
     public void checkStateAndCity() {
         $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(state));
         $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(city));
